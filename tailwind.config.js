206919fd97f60f6599c25071ce2c9041fb0f9b0f
bhansaliMadhav <bhansalimadhav03@gmail.com>
2024-11-10 11:49:09 +0530

    edited ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  darkMode: false, // This ensures dark mode is turned off
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#1f2937", // Customize the colors
+        secondary: "#f3f4f6",
       },
     },
   },
   plugins: [],
-}
+};
