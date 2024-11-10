117aded0fbeef8a276096855e3c5007cf80f51e9
bhansaliMadhav <bhansalimadhav03@gmail.com>
2024-11-10 11:52:44 +0530

    editing the ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: false, // This ensures dark mode is turned off
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "media",
   theme: {
     extend: {
-      colors: {
-        primary: "#1f2937", // Customize the colors
-        secondary: "#f3f4f6",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
